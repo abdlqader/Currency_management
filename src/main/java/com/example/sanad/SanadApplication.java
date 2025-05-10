64301fe7cfb7d74d2db2403f100706a32a6397db
abdulkader <mohamed@finflx.com>
2025-05-10 09:17:02 +0300

    initial commit with the following dependencies ( lombrok, postgres driver, redis driver, mapstruct, jpa, openfeign, openapi  )

---
+++ src/main/java/com/example/sanad/SanadApplication.java
@@ -0,0 +1,13 @@
+package com.example.sanad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sanad/SanadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sanad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
